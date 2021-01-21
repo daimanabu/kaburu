3366cbf5a84a1d2210af358953d6a27c2f029a6b
daimanabu <daimanabu777@gmail.com>
2021-01-21 12:02:55 +0900

    投稿機能の作成＆indexに表示

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,26 @@
 class PostsController < ApplicationController
+
   def index
+    @posts = Post.all
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
+
+
+  
 end
 
 
@@ -9,6 +29,6 @@ end
 
 private
 
-  def message_params
-    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+  def post_params
+    params.require(:post).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
